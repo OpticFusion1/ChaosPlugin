302289dacb54c4ed6ced0c47e4903552422dc4d0
Optic_Fusion1 <37254722+OpticFusion1@users.noreply.github.com>
2021-11-21 09:28:43 -0500

    Initial commit

---
+++ src/main/java/optic_fusion1/chaosplugin/ChaosPlugin.java
@@ -0,0 +1,15 @@
+package optic_fusion1.chaosplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChaosPlugin extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+  }
+
+  @Override
+  public void onDisable() {
+  }
+
+}
